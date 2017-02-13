c01da9de2498cb7efd8a9033c8962a356d41ea2a
Shinya Kumakura <info@kumakurashinya.com>
2017-02-13 14:05:18 +0900

    first commit

---
+++ js/common.js
@@ -0,0 +1,13 @@
+var i = 1;
+jQuery(function ($) {
+    $('.cont_02').hide();
+    $('.cont_03').hide();
+    $('.cont_04').hide();
+    $('#ClickBtn').on('click', function () {
+        i += 1;
+        $(`.cont_0${i}`).fadeIn('slow');
+        if (i == 4) {
+            $('#ClickBtn').hide();
+        };
+    });
+});
